b5956afed8f7194940d013e2392dea3f8953e07d
maker-ATOM <makersatom@gmail.com>
2023-10-06 19:58:28 +0530

    Breadth first Search

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "map_node = pathplanners.map:main"
+            "map_node = pathplanners.map:main",
+            "bfs = pathplanners.breadthfirstsearch:main"
         ],
     },
 )
